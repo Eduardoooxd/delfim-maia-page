69c907afa723eb1492b82bd564a72995fb2fcba7
Eduardo Couto <ecouto93@gmail.com>
2023-10-12 10:56:21 +0100

    feat: adding video as background

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -69,9 +69,6 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-      backgroundImage: {
-        "page-bg": "url('/src/assets/images/background.gif')",
-      },
       fontFamily: {
         sans: ["Garet", ...defaultTheme.fontFamily.sans],
       },
