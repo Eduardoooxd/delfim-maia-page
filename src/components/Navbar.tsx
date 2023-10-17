c91ea76baac1e2a950de105b4419e49942a1c996
Eduardo Couto <ecouto93@gmail.com>
2023-10-18 00:07:03 +0100

    feat: added mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,9 @@ import type { TRANSLATIONS_TYPE } from "@/i18n/ui";
 import { useTranslations } from "@/i18n/utils";
 import LogoImage from "@/images/logo.webp";
 import { cn } from "@/lib/utils";
+import { Menu, X } from "lucide-react";
 import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
 
 export interface NavLink {
   title: string;
@@ -19,6 +21,8 @@ export default function Navbar({ revealNavbar, language }: NavbarProps) {
   const [opacityToDisplay, setOpacityToDisplay] = useState<
     "opacity-100" | "opacity-0"
   >(!revealNavbar ? "opacity-100" : "opacity-0");
+
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const getOpacityToDisplay = () => {
     if (!revealNavbar) {
@@ -101,40 +105,86 @@ export default function Navbar({ revealNavbar, language }: NavbarProps) {
     };
   }, []);
 
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen((prevValue) => !prevValue);
+  };
+
+  const closeMobileMenu = () => {
+    setMobileMenuOpen(false);
+  };
+
   const translationContent = useTranslations(language);
   const { navlinks } = translationContent;
 
   return (
     <nav
       className={cn(
-        "fixed z-[100] top-0 left-0 w-full bg-white py-2 flex justify-between gap-4 items-center transition-opacity duration-300 ease-in",
+        "fixed z-[100] top-0 left-0 w-full bg-white py-2 flex flex-col gap-4 transition-opacity duration-300 ease-in",
         opacityToDisplay,
+        `${mobileMenuOpen ? "min-h-screen" : ""}`,
       )}
     >
-      <section className="px-8 sm:px-24">
-        <a href={`/${language}`}>
-          <img
-            className="w-[6.25rem] h-[4,875rem]"
-            src={LogoImage.src}
-            alt="Image do logo de Delfim Maia"
-          />
-        </a>
-      </section>
-
-      <section className="flex-col gap-1 hidden sm:flex">
-        <section className="flex gap-8 pr-8 sm:pr-24">
-          {navlinks.map(({ title, href, name }) => (
-            <a
-              className="capitalize font-bold text-sm sm:text-base hover:text-delfim-primary transition-colors"
-              section-name={name}
-              href={`/${language}${href}`}
-              key={name}
-            >
-              {title}
-            </a>
-          ))}
+      <div className="flex justify-between gap-4 items-center">
+        <section className="px-8 sm:px-24">
+          <a href={`/${language}`}>
+            <img
+              className="w-[6.25rem] h-[4,875rem]"
+              src={LogoImage.src}
+              alt="Image do logo de Delfim Maia"
+            />
+          </a>
         </section>
-        <div className="border-b-4 border-navbar-border"></div>
+
+        <section className="flex-col gap-1 hidden sm:flex">
+          <section className="flex gap-8 pr-8 sm:pr-24">
+            {navlinks.map(({ title, href, name }) => (
+              <a
+                className="capitalize font-bold text-sm sm:text-base hover:text-delfim-primary transition-colors"
+                section-name={name}
+                href={`/${language}${href}`}
+                key={name}
+              >
+                {title}
+              </a>
+            ))}
+          </section>
+          <div className="border-b-4 border-navbar-border"></div>
+        </section>
+
+        <section className="sm:hidden px-8">
+          <Button
+            onClick={() => toggleMobileMenu()}
+            variant="ghost"
+            size="icon"
+          >
+            {mobileMenuOpen ? (
+              <X className="text-delfim-primary h-8 w-8" />
+            ) : (
+              <Menu className="text-delfim-primary h-8 w-8" />
+            )}
+          </Button>
+        </section>
+      </div>
+
+      <section
+        className={cn(
+          "flex-col gap-4 px-8",
+          `${mobileMenuOpen ? "flex" : "hidden sm:hidden"}`,
+        )}
+      >
+        {navlinks.map(({ title, href, name }) => (
+          <a
+            className="capitalize font-bold text-xl"
+            section-name={name}
+            href={`/${language}${href}`}
+            onClick={() => {
+              closeMobileMenu();
+            }}
+            key={name}
+          >
+            {title}
+          </a>
+        ))}
       </section>
     </nav>
   );
