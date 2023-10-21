92b1302d8343bdae6e02dc8ca948d53e6fed3a25
Eduardo Couto <ecouto93@gmail.com>
2023-10-21 16:22:29 +0100

    feat: added cms support for team members

---
+++ src/components/TeamMember.ts
@@ -0,0 +1,9 @@
+export interface TeamMember {
+  teamMemberId: string;
+  name: string;
+  image: string;
+  rolePt: string;
+  roleEn: string;
+  descriptionEn: string;
+  descriptionPt: string;
+}
