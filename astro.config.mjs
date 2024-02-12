c5ccf959831fcbdda3bf6f3e1c0ec35cb42b59d3
Eduardo Couto <ecouto93@gmail.com>
2024-02-12 17:49:29 +0000

    Update Astro configuration and AboutUsSection styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		react(),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-	],
+  image: {
+    domains: ["builder.io"],
+  },
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
